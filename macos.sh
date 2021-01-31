93279a764f00bdbf18d8c4890a869999e4be7e37
Denis Z <zzulev@gmail.com>
2021-01-31 12:13:45 +0100

    Enable notifications center

--- macos.sh
+++ macos.sh
@@ -36,9 +36,6 @@ defaults write com.apple.helpviewer DevMode -bool true
 # in the login window
 sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
 
-# Disable Notification Center and remove the menu bar icon
-launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
-
 # Disable automatic capitalization as it’s annoying when typing code
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 

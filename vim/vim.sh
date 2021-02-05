83dca3aca9950feec3e48a22b32a39ee35d1bc30
Denis Z <zzulev@gmail.com>
2021-02-05 21:41:25 +0100

    Update vim config

--- vim/vim.sh
+++ vim/vim.sh
@@ -2,13 +2,7 @@
 dotfiles_dir=~/dev/dotfiles
 
 # Install
-# if nvim -V >/dev/null 2>&1; then
-#   echo "neovim has been already installed, skipping installation"
-# else
-#   brew install neovim
-# fi
 brew install vim
-brew link vim
 
 # Vim plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -17,3 +11,13 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 # Symlinks
 rm -f ~/.vimrc
 ln -s $dotfiles_dir/vim/.vimrc ~/.vimrc
+
+# Neovim
+brew instll neovim
+
+# Symlinks for neovim
+ln -s ~/dev/dotfiles/vim/.vimrc ~/.config/nvim/init.vim
+
+# Vim plug for neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
